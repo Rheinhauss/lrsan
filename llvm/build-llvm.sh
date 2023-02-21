1dcafaebbd5d07fa0b9d1dc6047066a0b757c0c5
Your Name <you@example.com>
2023-02-21 16:30:59 +0800

    amend

--- llvm/build-llvm.sh
+++ llvm/build-llvm.sh
@@ -7,14 +7,14 @@
 # download pre-built package 
 # PKGN=clang+llvm-$VERN-$DIST
 # TARF=$PKGN.tar.xz
-TARF="/tmp/clang+llvm-7.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz"
+# TARF="/tmp/clang+llvm-7.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz"
 
 # LINK=$BASE/$VERN/$TARF
 # wget $LINK
 
 # extract to location
-tar xvf $TARF
-mv $PKGN build
+# tar xvf $TARF
+mv "clang+llvm-7.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz" build
 
 # remove file
-rm $TARF
+# rm $TARF
