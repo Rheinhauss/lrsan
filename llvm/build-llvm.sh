9df60d8a887d3d3c89df448ba77e9551f643e801
Your Name <you@example.com>
2023-02-21 15:24:20 +0800

    add offline build version

--- llvm/build-llvm.sh
+++ llvm/build-llvm.sh
@@ -1,35 +1,20 @@
-#!/bin/bash
+﻿#!/bin/bash
 
 # binary choices
-BASE="http://releases.llvm.org"
-VERN="7.0.0"
-
-# platform specifics
-case "$OSTYPE" in
-  linux*)
-    DIST="x86_64-linux-gnu-ubuntu-16.04"
-    ;;
-  darwin*)
-    DIST="x86_64-apple-darwin"
-    ;;
-  bsd*)
-    DIST="amd64-unknown-freebsd10"
-    ;;
-  *)
-    echo "Unknown OS ($OSTYPE)"; exit 
-    ;;
-esac
+# BASE="http://releases.llvm.org"
+# VERN="7.0.0"
 
 # download pre-built package 
-PKGN=clang+llvm-$VERN-$DIST
-TARF=$PKGN.tar.xz
+# PKGN=clang+llvm-$VERN-$DIST
+# TARF=$PKGN.tar.xz
+TARF="/share/clang+llvm-7.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz"
 
-LINK=$BASE/$VERN/$TARF
-wget $LINK
+# LINK=$BASE/$VERN/$TARF
+# wget $LINK
 
 # extract to location
 tar xvf $TARF
 mv $PKGN build
 
 # remove file
-rm $TARF
+# rm $TARF
